68f8c8c1b109aae2ed84b050df2637fc25799144
xjaduhx <112858162+xjaduhx@users.noreply.github.com>
2022-11-22 11:18:09 +0100

    Eevee h1
    h1 toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,1 @@
-var name = "henk";
-var age = 20;
-var dobbel = ['Jade', 'Elisa', 'Hardy', 'Merel'];
-
-var persoon = {
-    name: 'Jan',
-    age: 32,
-    auto: false,
-    glas: ['kort', 'lang']
-}
-
-if (persoon.age >= 18) {
-    console.log("Jij mag drinken", dobbel[0], persoon.name);
-} else {
-    console.log("Jij mag niet drinken")
-}
 
