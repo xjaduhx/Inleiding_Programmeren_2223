4e41dbd570b6f7d38805aae1205ff1099e3888b6
xjaduhx <112858162+xjaduhx@users.noreply.github.com>
2022-12-08 12:14:24 +0100

    de balk doet het nu nog beter

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,4 @@
-// console.log("Hallo Eevee");
-
-// var feedbutton = document.querySelector("button")
-// console.log(feedbutton);
-
-// var sleepbutton = document.querySelector("ul:nth-of-type(1) li:nth-of-type(2) button")
-// console.log(sleepbutton);
-
-// var petbutton = document.querySelector(" ul:nth-of-type(1) li:nth-of-type(3) button")
-// console.log(petbutton);
-
 var balkStatus = 0;
-// var balkStatusEvolutie1 = 0
 var welkeEvolutie = 0
 var imageElement = document.querySelector('.debalk');
 var eeveePlaatje = document.querySelector('.eevee');
@@ -49,65 +37,25 @@ function updateBalk(){
     }
 
     else{
-        imageElement.src = "./images/bar0.png";
-        eeveePlaatje.src = "./images/sylveon.jpeg";
-        balkStatus = 0;
-        welkeEvolutie = 1;
-        console.log("werkt dit?")
 
-        if (welkeEvolutie == 1){
+        if (welkeEvolutie == 0){
             eeveePlaatje.src = "./images/sylveon.jpeg";
-            welkeEvolutie = 2;
+            welkeEvolutie = 1;
+            imageElement.src = "./images/bar0.png";
+            balkStatus = 0;
+            console.log("werkt dit?")
         }
 
-        else if (welkeEvolutie == 2){
+        else if (welkeEvolutie == 1){
             eeveePlaatje.src = "./images/umbreon.jpeg";
-            welkeEvolutie = 3;
+            welkeEvolutie = 2;
+            balkStatus = 0;
+            imageElement.src = "./images/bar0.png";
         }
     }
 }
 
 
-
-// function updateBalkEersteEvolutie(){
-//     if (balkStatus == 0){
-//         imageElement.src = "./images/Bar2.png";
-//         balkStatus = 20;
-//         console.log("ga de balk updaten")
-//     }
-
-//     else if (balkStatus == 20){
-//         imageElement.src = "./images/Bar3.png";
-//         balkStatus = 40;
-//         console.log("ga de balk updaten2")
-//     }
-
-//     else if (balkStatus == 40){
-//         imageElement.src = "./images/Bar4.png";
-//         balkStatus = 60;
-//         console.log("ga de balk updaten3")
-//     }
-
-//     else if (balkStatus == 60){
-//         imageElement.src = "./images/Bar5.png";
-//         balkStatus = 80;
-//         console.log("ga de balk updaten4")
-//     }
-    
-//     else if (balkStatus == 80){
-//         imageElement.src = "./images/Bar6.png";
-//         balkStatus = 100;
-//         console.log("ga evalueren!")
-//     }
-
-//     else{
-//         imageElement.src = "./images/bar0.png"
-//         eeveePlaatje.src = "./images/bar0.png" /*Wordt een plaatje van eerste Eevee evolutie*/
-//         balkStatus = 0
-//         welkeEvolutie = 2
-//         console.log("werkt dit?")
-//     }
-// }   
 
 
 var eersteKnop = document.querySelector('.eerste_knop');
