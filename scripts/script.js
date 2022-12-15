38f27d9ff2c5299dc3e863c5eb5b0d00f754c316
xjaduhx <112858162+xjaduhx@users.noreply.github.com>
2022-12-15 11:24:07 +0100

    Array werkt

--- scripts/script.js
+++ scripts/script.js
@@ -2,38 +2,57 @@ var balkStatus = 0;
 var welkeEvolutie = 0
 var imageElement = document.querySelector('.debalk');
 var eeveePlaatje = document.querySelector('.eevee');
-var inputField = document.querySelector('.input_field');
-var submitButton = document.querySelector('.submit_button');
-console.log(submitButton);
+
+
 function updateBalk(){
     if (balkStatus == 0){
         imageElement.src = "./images/Bar2.png";
         balkStatus = 20;
         console.log("ga de balk updaten")
+        document.querySelector('.eerste_knop').disabled = false;
+        document.querySelector('.tweede_knop').disabled = false;
+        document.querySelector('.derde_knop').disabled = false;
+        document.querySelector('#evolvebuttonid').disabled = true;
     }
 
     else if (balkStatus == 20){
         imageElement.src = "./images/Bar3.png";
         balkStatus = 40;
         console.log("ga de balk updaten2")
+        document.querySelector('.eerste_knop').disabled = false;
+        document.querySelector('.tweede_knop').disabled = false;
+        document.querySelector('.derde_knop').disabled = false;
+        document.querySelector('#evolvebuttonid').disabled = true;
     }
 
     else if (balkStatus == 40){
         imageElement.src = "./images/Bar4.png";
         balkStatus = 60;
         console.log("ga de balk updaten3")
+        document.querySelector('.eerste_knop').disabled = false;
+        document.querySelector('.tweede_knop').disabled = false;
+        document.querySelector('.derde_knop').disabled = false;
+        document.querySelector('#evolvebuttonid').disabled = true;
     }
 
     else if (balkStatus == 60){
         imageElement.src = "./images/Bar5.png";
         balkStatus = 80;
         console.log("ga de balk updaten4")
+        document.querySelector('#evolvebuttonid').disabled = true;
+        document.querySelector('.eerste_knop').disabled = false;
+        document.querySelector('.tweede_knop').disabled = false;
+        document.querySelector('.derde_knop').disabled = false;
     }
     
     else if (balkStatus == 80){
         imageElement.src = "./images/Bar6.png";
         balkStatus = 100;
         console.log("ga evalueren!")
+        document.querySelector('.eerste_knop').disabled = true;
+        document.querySelector('.tweede_knop').disabled = true;
+        document.querySelector('.derde_knop').disabled = true;
+        document.querySelector('#evolvebuttonid').disabled = false;
     }
 
     else{
@@ -44,6 +63,11 @@ function updateBalk(){
             imageElement.src = "./images/bar0.png";
             balkStatus = 0;
             console.log("werkt dit?")
+            document.querySelector('#evolvebuttonid').disabled = true;
+            document.querySelector('.eerste_knop').disabled = false;
+            document.querySelector('.tweede_knop').disabled = false;
+            document.querySelector('.derde_knop').disabled = false;
+            // eventesten = 0;
         }
 
         else if (welkeEvolutie == 1){
@@ -57,24 +81,77 @@ function updateBalk(){
 function handleSubmit(){
     console.log('hallo');
 }
-// function barking(){
-//     document.getElementById('barking').play();
-// }
+function barking(){
+    document.getElementById('barking').play();
+}
 
-submitButton.addEventListener('submit', function(e){
-    e.preventDefault()
-    console.log('hallo');
+function sleepeevee(){
+    eeveePlaatje.src = "./images/eeveesleep.PNG";
+}
+
+function feedeevee(){
+    eeveePlaatje.src = "./images/eeveefood.PNG";
+}
+
+function playeevee(){
+    eeveePlaatje.src = "./images/eeveeplay.PNG";
+}
+
+
+
+
+
+/* Dit is het stukje waarbij als de gebruiker een naam invult, de h2 vervanderd in de ingevulde naam*/
+var input = document.querySelector("input");
+var p = document.querySelector(".jouwnaam");
+var button = document.querySelector(".naamkiezen");
+button.addEventListener("click", () => {
+    p.textContent = input.value;
 });
 
 
 
+
+
+/*Array*/
+var buttonDing = document.querySelector('.randomspeelgoed');
+buttonDing.addEventListener("click", randomPlaatje);
+
+function randomPlaatje(){
+    var imageChange = ['./images/voetbal.png', './images/champagne.png', './images/sylveon.jpeg'];
+    var randomIndex = Math.floor(Math.random()*3);
+    document.querySelector('.speelgoedje').src = imageChange[randomIndex];
+}
+
+
+
+
+/*Deze variable worden aangemaakt om ervoor te zorgen dat de button classes luisteren naar de eventlistener, nameliijk de click op de button*/ 
 var eersteKnop = document.querySelector('.eerste_knop');
 eersteKnop.addEventListener('click', updateBalk,);
-// eersteKnop.addEventListener('click', barking);
+eersteKnop.addEventListener('click', feedeevee);
 
 var eersteKnop = document.querySelector('.tweede_knop');
 eersteKnop.addEventListener('click', updateBalk);
+eersteKnop.addEventListener('click', sleepeevee);
 
 var eersteKnop = document.querySelector('.derde_knop');
 eersteKnop.addEventListener('click', updateBalk);
+eersteKnop.addEventListener('click', playeevee);
+eersteKnop.addEventListener('click', barking);
+
+
+
+
+
+var eventesten = document.querySelector("#evolvebuttonid");
+eventesten.addEventListener('click', updateBalk) 
+/* Als je op de button klikt van evolve, evolved eevee wel naar sylveon. Maar als je dan nog een keer op een button klikt gaat
+hij weer terug naar eevee in plaats van verder met sylveon. Dit komt omdat ik een eventlistener heb gehangen aan de buttons.*/
+console.log(eventesten);
+if (balkStatus == 100) {
+    eventesten.removeAttribute("disabled")
+};
+
+
 
